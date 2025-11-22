a6079030874eb7ddf2a9cab2123c20d062ef4f65
emiandd <emilandresaravena@icloud.com>
2025-11-21 21:25:25 -0300

    fix: db connection

--- db/mysql/connector.go
+++ db/mysql/connector.go
@@ -15,7 +15,7 @@ var (
 type Connector struct{}
 
 func (co Connector) Open(c *gin.Context) (*sql.DB, error) {
-	db, err := sql.Open("mysql", os.Getenv("USER_DB")+"@tcp("+ os.Getenv("HOST_DB") +":"+ os.Getenv("PORT_DB") +")/"+os.Getenv("NAME_DB")+"?parseTime=true")
+	db, err := sql.Open("mysql", os.Getenv("USER_DB")+":" + os.Getenv("PASS_DB") + "@tcp("+ os.Getenv("HOST_DB") +":"+ os.Getenv("PORT_DB") +")/"+os.Getenv("NAME_DB")+"?parseTime=true")
 	if err != nil {
 		return nil, err
 	}
